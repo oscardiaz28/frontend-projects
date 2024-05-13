24921a456b85d3dd2a0b8a2deabc7a15dd3f89bd
oscardiaz28 <diazvargasod@gmail.com>
2024-05-12 21:10:06 -0500

    todo: terminar el sidebar

--- js/app.js
+++ js/app.js
@@ -33,9 +33,7 @@
         elem.classList.add("active");
     }
 
-
-   
-
+    
 
 
 })()
---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+( () => {
+
+    const aside = document.querySelector('.aside');
+    const btn = document.querySelector('.navbar_left > a');
+    
+
+
+})()
